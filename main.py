f2eaddb1b87683506bedc6c6c83e5f7e9124ba44
Edward Xu <edxu96@outlook.com>
2022-05-28 20:35:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 
-from os import environ
+import os
+import pathlib
+import zipfile
 
 from loguru import logger as _LOG
+import requests
 from telegram import ForceReply, Update
 from telegram.ext import Application, CallbackContext, CommandHandler, MessageHandler, filters
 
-_TOKEN: str = environ.get('CMDICT_BOT')
+_TOKEN: str = os.environ.get('CMDICT_BOT')
+_DB_URL = "https://github.com/skywind3000/ECDICT/releases/download/1.0.28/ecdict-sqlite-28.zip"  # noqa: E501
+
+
+_db_dir = os.path.join(str(pathlib.Path(__file__).parent), "data")
+_db_zip = os.path.join(_db_dir, "stardict.zip")
+
+
+def _download_stardict():
+    data_dir_path = pathlib.Path(_db_dir)
+    if not data_dir_path.exists():
+        data_dir_path.mkdir(parents=True)
+
+    r = requests.get(_DB_URL, stream=True)
+    block_size = 1024
+
+    _LOG.info("Start to download \"stardict.zip\".")
+
+    with open(_db_zip, "wb") as f:
+        for data in r.iter_content(block_size):
+            f.write(data)
+
+    with zipfile.ZipFile(_db_zip, "r") as ref:
+        ref.extractall(_db_dir)
+
+    _LOG.success("\"stardict.zip\" has been downloaded and extracted.")
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -31,20 +59,24 @@ async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
 
-def main() -> None:
-    """Start the bot."""
+def _start_app():
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(_TOKEN).build()
+    app = Application.builder().token(_TOKEN).build()
 
     # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     # Run the bot until the admin presses Ctrl-C
-    application.run_polling()
+    app.run_polling()
+
+
+def main() -> None:
+    _download_stardict()
+    _start_app()
 
 
 if __name__ == "__main__":
