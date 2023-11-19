dfccdc960155190479aeee3c23eb3db0d597eb50
Edward Xu <edxu96@outlook.com>
2023-11-19 15:56:21 +0100

    move `requirements.txt`

--- poetry.sh
+++ poetry.sh
@@ -3,6 +3,6 @@
 
 poetry update --with dev
 
-poetry export -f requirements.txt --output cmdict_bot/requirements.txt --without-hashes
+poetry export -f requirements.txt --output requirements.txt --without-hashes
 
 poetry export -f requirements.txt --output tests/requirements.txt --only=dev --without-hashes
