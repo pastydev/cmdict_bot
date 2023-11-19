095eb8ad634d3d9ec41568825fc7c15318f02992
Edward Xu <edxu96@outlook.com>
2023-11-19 17:15:33 +0100

    Update test_api_gateway.py

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -4,26 +4,23 @@ import boto3
 import pytest
 import requests
 
-"""
-Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
-"""
+_REGION_NAME = "eu-central-1"
+
+_STACK_NAME = os.environ.get("AWS_SAM_STACK_NAME")
+if _STACK_NAME is None:
+    raise ValueError('Please set the AWS_SAM_STACK_NAME environment variable to the name of your stack')
 
 
 @pytest.fixture()
 def api_gateway_url():
     """ Get the API Gateway URL from Cloudformation Stack outputs """
-    stack_name = os.environ.get("AWS_SAM_STACK_NAME")
-
-    if stack_name is None:
-        raise ValueError('Please set the AWS_SAM_STACK_NAME environment variable to the name of your stack')
-
-    client = boto3.client("cloudformation")
+    client = boto3.client("cloudformation", region_name=_REGION_NAME)
 
     try:
-        response = client.describe_stacks(StackName=stack_name)
+        response = client.describe_stacks(StackName=_STACK_NAME)
     except Exception as e:
         raise Exception(
-            f"Cannot find stack {stack_name} \n" f'Please make sure a stack with the name "{stack_name}" exists'
+            f"Cannot find stack {_STACK_NAME} \n" f'Please make sure a stack with the name "{_STACK_NAME}" exists'
         ) from e
 
     stacks = response["Stacks"]
@@ -31,7 +28,7 @@ def api_gateway_url():
     api_outputs = [output for output in stack_outputs if output["OutputKey"] == "HelloWorldApi"]
 
     if not api_outputs:
-        raise KeyError(f"HelloWorldAPI not found in stack {stack_name}")
+        raise KeyError(f"HelloWorldAPI not found in stack {_STACK_NAME}")
 
     return api_outputs[0]["OutputValue"]  # Extract url from stack outputs
 
