7c90483fcd748e7688528bf1807f7e252ffdc73a
Edward Xu <edxu96@outlook.com>
2023-11-19 15:05:48 +0100

    Update poetry.sh

--- poetry.sh
+++ poetry.sh
@@ -3,4 +3,4 @@
 
 poetry update --with dev
 
-poetry export -f requirements.txt --output requirements.txt --without-hashes
+poetry export -f requirements.txt --output cmdict_bot/requirements.txt --without-hashes
