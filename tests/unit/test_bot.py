946acd7634ee54f2d4d7d646d1c25bc23dea078d
Edward Xu <edxu96@outlook.com>
2023-11-19 17:12:48 +0100

    move

--- tests/unit/test_bot.py
+++ tests/unit/test_bot.py
@@ -1,11 +1,11 @@
 """Test the Telegram bot."""
 from os import environ
 
-from cmdict_bot.bot import start_bot
+from cmdict_bot.bot import config_app
 
 _TOKEN: str = environ.get("CMDICT_TEST_BOT")
 
 
 def test_start_bot():
     """Test if the bot can be started correctly."""
-    start_bot(_TOKEN)
+    config_app(_TOKEN)
